181b7983dd115494db26c02cd72772406086a875
duaraghav8@gmail <duaraghav8@gmail.com>
2015-09-21 11:21:28 +0530

    Bug Fixes

--- snakes_and_ladders.py
+++ snakes_and_ladders.py
@@ -2,7 +2,6 @@ class Case:
 	def __init__ (self):
 		self.graph = {};
 		for i in range (1, 100):
-#			self.graph [str (i)] = set ([str (i + 1)]);
 			self.graph [i] = set ([i + 1]);
 
 		self.ladderNum = int (raw_input ());
@@ -11,13 +10,14 @@ class Case:
 		for i in range (0, self.ladderNum):
 			ladder = [int (i) for i in raw_input ().split ()];
 			self.ladders.append (ladder);
-#			ladder = raw_input ().split ();
 			self.graph [ladder [0]].add (ladder [1]);
 
 		self.snakeNum = int (raw_input ());
+		self.snakes = [];
+
 		for i in range (0, self.snakeNum):
 			snake = [int (i) for i in raw_input ().split ()];
-#			snake = raw_input ().split ();
+			self.snakes.append (snake);
 			self.graph [snake [0]].add (snake [1]);
 
 	def getLeastRolls (self):
@@ -26,18 +26,33 @@ class Case:
 		lengths = [len (i) for i in paths];
 		shortest = [int (i) for i in paths [lengths.index (min (lengths))]];
 		lengthLeft = 6;
+		ladderStartPoints = [ladder [0] for ladder in self.ladders];
+		snakeStartPoints = [snake [0] for snake in self.snakes];
 
 		for i in range (1, len (shortest)):
-			if (self.ladders.count ([shortest [i-1], shortest [i]]) == 0):
+			#have to account for a snake existing en route
+			if (not i == len (shortest) - 1 and snakeStartPoints.count (shortest [i+1]) > 0 and lengthLeft == 2):
+				rolls += 1;
+				lengthLeft = 6;
+				print ("snake ", rolls, shortest [i]);
+				continue;
+
+			if ( (self.ladders.count ([shortest [i-1], shortest [i]]) == 0) and (self.snakes.count ([shortest [i-1], shortest [i]]) == 0) ):
 				if (shortest [i] - shortest [i-1] <= lengthLeft):
 					lengthLeft -= shortest [i] - shortest [i-1];
 					if (lengthLeft == 0 or shortest [i] == 100):
 						lengthLeft = 6;
-						rolls += 1;
+						if (ladderStartPoints.count (shortest [i]) == 0):
+							rolls += 1;
 			else:
 				rolls += 1;
 				lengthLeft = 6;
-					
+
+			print (rolls, shortest [i]);
+
+#		print (paths);
+		print (lengths);
+		print (shortest);
 		return (rolls);
 			
 
