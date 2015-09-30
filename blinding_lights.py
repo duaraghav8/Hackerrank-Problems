e5a9acc3c1a794d12c02db272041c95feb50b930
Raghav Dua <duaraghav8@gmail.com>
2015-09-30 23:30:30 +0530

    Removed unnecessary OOP for optimality

--- blinding_lights.py
+++ blinding_lights.py
@@ -1,23 +1,3 @@
-class Graph (object):
-	def __init__ (self):
-		self.numOfNodes, self.numOfEdges = [int (i) for i in input ().split ()];
-		self.graph = [ [float ('inf') for j in range (0, self.numOfNodes)] for i in range (0, self.numOfNodes) ];
-		self.distances = None;
-
-		for i in range (0, self.numOfNodes):
-			self.graph [i] [i] = 0;
-
-		for i in range (0, self.numOfEdges):
-			edge = [int (i) for i in input ().split ()];
-			self.graph [edge [0] - 1] [edge [1] - 1] = edge [2];
-
-	def shortest_path (self, start, target):
-		if (not self.distances):
-			self.distances = self.graph;
-			self.distances = floyd_warshall (self.distances);
-
-		return (self.distances [start - 1] [target - 1]);
-
 def floyd_warshall (graph):
 	nodes = len (graph);
 	for k in range (0, nodes):
@@ -26,15 +6,26 @@ def floyd_warshall (graph):
 				if (graph [i] [k] + graph [k] [j] < graph [i] [j]):
 					graph [i] [j] = graph [i] [k] + graph [k] [j];
 	return (graph);
-
 if (__name__) == '__main__':
-	graph = Graph ();
-	n = int (input ());
+	numOfNodes, numOfEdges = [int (i) for i in input ().split ()];
+	graph = [];
 	queries = [];
+	for i in range (numOfNodes):
+		row = [];
+		for j in range (numOfNodes):
+			if (i == j):
+				row.append (0);
+			else:
+				row.append (float ('inf'));
+		graph.append (row);
+	for i in range (0, numOfEdges):
+		edge = [int (i) for i in input ().split ()];
+		graph [edge [0] - 1] [edge [1] - 1] = edge [2];
 
-	for i in range (0, n):
+	numOfQueries = int (input ());
+	for i in range (numOfQueries):
 		queries.append ([int (i) for i in input ().split ()]);
-
+	distances = floyd_warshall (graph);
 	for query in queries:
-		dist = graph.shortest_path (query [0], query [1]);
+		dist = distances [query [0] - 1] [query [1] - 1];
 		print ('-1' if dist == float ('inf') else dist);
