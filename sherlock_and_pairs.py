e155261d062d040446e707193460ce10baa84337
Raghav Dua <duaraghav8@gmail.com>
2015-10-13 22:57:05 +0530

    Reduced O(N^2) to O(N) - All Passed

--- sherlock_and_pairs.py
+++ sherlock_and_pairs.py
@@ -1,9 +1,20 @@
 t = int (input ());
-cases = [ (int (input()), [int (i) for i in input ().split ()]) for i in range (t) ];
+cases = [];
+
+for i in range (t):
+	size = int (input ());
+	d = {};
+
+	vals = [int (i) for i in input ().split ()];
+	for value in vals:
+		try:
+			d [value] += 1;
+		except Exception as e:
+			d [value] = 1;
+	cases.append ( (size, d) );
 
 for case in cases:
 	total = 0;
-	for i in set (case [1]):
-		n = case [1].count (i);
-		total += (n * (n - 1)) if n >= 2 else 0;
+	for num in case [1]:
+		total += case [1] [num] * (case [1] [num] - 1) if case [1] [num] >= 2 else 0;
 	print (total);
