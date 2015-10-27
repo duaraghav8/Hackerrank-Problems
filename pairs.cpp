378cab5d8684cccdab52887bde681f0b69d7de6e
Raghav Dua <duaraghav8@gmail.com>
2015-10-27 21:33:53 +0530

    Update pairs.cpp

--- pairs.cpp
+++ pairs.cpp
@@ -8,6 +8,12 @@ using namespace std;
 
 int pair_count (vector< int >& array, map< int, int >& myMap, int& diff) {
 	int result (0);
+	/*
+	we look at myMap key-by-key.
+	If key = 5 and diff = 2 (see the test case), then we know that we should either be looking for a 3 or a 7.
+	So we follow a convention of only looking upward in the number line, i.e., when we encounter 5, we only look to see if 7 exists.
+	Don't think we are skipping the 3. If 3 exists somewhere later in the map, 3 will be looking for 5 (which exists).
+	*/
 	for (auto i : array) {
 		if (myMap [abs (i + diff)] == 1) { result++; }
 	}
